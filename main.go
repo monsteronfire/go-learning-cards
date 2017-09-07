6c68311cb6c80d3bf5fb80bf113f23fc8cda2985
Shaii Ong <monsteronfire@gmail.com>
2017-09-07 21:18:35 +0800

    Create a slice of cards

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	//var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
-	fmt.Println(card)
+	cards := []string{newCard(), newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
